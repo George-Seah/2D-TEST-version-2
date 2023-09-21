3c509f5f909fc7e07cebb837568effb208fdd6c4
George-Seah <georgerseah@gmail.com>
2023-09-21 19:07:09 +0800

    Fixed collision, A/D flip, improved jump physics and more.

---
+++ 2D TEST/Assets/Scripts/CharacterAnimator.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterAnimator : MonoBehaviour
+{
+
+    Animator anim;
+    SpriteRenderer spriteRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+        spriteRend = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow) || Input.GetKey(KeyCode.RightArrow) || Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.D)){
+            anim.SetBool("isRunning", true);
+            anim.SetFloat("horizontalDirection", PlayerBehavior.horizontalAxis);
+            /*
+            if(Input.GetKey(KeyCode.LeftArrow)){
+                spriteRend.flipX = true;
+            }
+            if(Input.GetKey(KeyCode.RightArrow)){
+                spriteRend.flipX = false;
+            }
+            */
+            if(PlayerBehavior.horizontalAxis < 0){
+                spriteRend.flipX = true;
+            }
+            else if(PlayerBehavior.horizontalAxis > 0){
+                spriteRend.flipX = false;
+            }
+            
+        } else{
+            anim.SetBool("isRunning", false);
+        }
+
+        if(Input.GetKeyDown(KeyCode.Space)){
+            anim.SetTrigger("jump");
+        }
+    }
+}
--- 2D TEST/Assets/Scripts/PlayerBehavior.cs
+++ 2D TEST/Assets/Scripts/PlayerBehavior.cs
@@ -5,10 +5,13 @@ using UnityEngine;
 
 public class PlayerBehavior : MonoBehaviour
 {
-    float horizontalAxis;
-    float verticalPosition;
+    public static float horizontalAxis;
+    [SerializeField] float verticalPosition;
+    [SerializeField] float speed = 5;
     Rigidbody2D rb;
     public float jumpAmount = 10;
+    public float gravityScale = 10;
+    public float fallingGravityScale = 40;
     // Start is called before the first frame update
     void Start()
     {
@@ -19,9 +22,15 @@ public class PlayerBehavior : MonoBehaviour
     void Update()
     {
         horizontalAxis = Input.GetAxis("Horizontal");
-        transform.position += new Vector3(horizontalAxis * Time.deltaTime * 5, 0, 0);
+        transform.position += new Vector3(horizontalAxis * Time.deltaTime * speed, 0, 0);
         if(Input.GetKeyDown(KeyCode.Space)){
             rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+        }
+        if(rb.velocity.y >= 0){
+            rb.gravityScale = gravityScale;
+        }
+        else if(rb.velocity.y < 0){
+            rb.gravityScale = fallingGravityScale;
         }
     }
 }
