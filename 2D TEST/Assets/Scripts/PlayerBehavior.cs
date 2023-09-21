924d8f452098f286cf609e3ec4f001cf0854cd43
George-Seah <georgerseah@gmail.com>
2023-09-22 02:34:44 +0800

    Polished jump.

--- 2D TEST/Assets/Scripts/PlayerBehavior.cs
+++ 2D TEST/Assets/Scripts/PlayerBehavior.cs
@@ -8,14 +8,24 @@ public class PlayerBehavior : MonoBehaviour
     public static float horizontalAxis;
     [SerializeField] float verticalPosition;
     [SerializeField] float speed = 5;
+
     Rigidbody2D rb;
-    public float jumpAmount = 10;
+
     public float gravityScale = 10;
     public float fallingGravityScale = 40;
+    public float buttonTime = 0.3f;
+    float jumpTime;
+    bool jumping;
+    
+    public float jumpAmount = 1.25f;
+
+    float jumpForce;
+
     // Start is called before the first frame update
     void Start()
     {
         rb = GetComponent<Rigidbody2D>();
+        jumpForce = Mathf.Sqrt(jumpAmount * -2 * (Physics2D.gravity.y * rb.gravityScale));
     }
 
     // Update is called once per frame
@@ -25,6 +35,17 @@ public class PlayerBehavior : MonoBehaviour
         transform.position += new Vector3(horizontalAxis * Time.deltaTime * speed, 0, 0);
         if(Input.GetKeyDown(KeyCode.Space)){
             rb.AddForce(Vector2.up * jumpAmount, ForceMode2D.Impulse);
+            jumping = true;
+            jumpTime = 0;
+            
+        }
+        if(Input.GetKeyUp(KeyCode.Space) | jumpTime > buttonTime){
+            jumping = false;
+        }
+        if(jumping){
+            //rb.AddForce(new Vector2(0, jumpForce), ForceMode2D.Impulse);
+            rb.velocity = new Vector2(rb.velocity.x, jumpAmount);
+            jumpTime += Time.deltaTime;
         }
         if(rb.velocity.y >= 0){
             rb.gravityScale = gravityScale;
@@ -32,5 +53,6 @@ public class PlayerBehavior : MonoBehaviour
         else if(rb.velocity.y < 0){
             rb.gravityScale = fallingGravityScale;
         }
+        
     }
 }
