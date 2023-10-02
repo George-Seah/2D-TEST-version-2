d4e444db081f87e8949e01757d5c4ecf867f98dc
George-Seah <georgerseah@gmail.com>
2023-10-02 19:28:53 +0800

    Merged PlayerDeath script with PlayerHealth script.

--- 2D TEST/Assets/Scripts/PlayerHealth.cs
+++ 2D TEST/Assets/Scripts/PlayerHealth.cs
@@ -2,12 +2,14 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
     public int maxHealth = 5;
     public static int currentHealth = 3;
-
+    
+    private Rigidbody2D rb;
     private Animator anim;
 
     // Start is called before the first frame update
@@ -15,6 +17,7 @@ public class PlayerHealth : MonoBehaviour
     {
         currentHealth = 3;
         anim = GetComponent<Animator>();
+        rb = GetComponent<Rigidbody2D>();
     }
 
     public void TakeDamage(int amount){
@@ -24,6 +27,7 @@ public class PlayerHealth : MonoBehaviour
             //Player is dead
             //Play Death Animation
             //Show GameOver screen
+            Die();
         }
     }
 
@@ -33,6 +37,26 @@ public class PlayerHealth : MonoBehaviour
         if(currentHealth > maxHealth){
             currentHealth = maxHealth;
         }
+    }
+
+    /*
+    private void OnCollisionEnter2D(Collision2D collision){
+        if(collision.gameObject.CompareTag("Trap")){
+            Die();
+        }
+    }
+    */
+    private void Die(){
+        rb.bodyType = RigidbodyType2D.Static;
+        anim.SetTrigger("death");
+        PlayerBehavior.alive = false;
+    }
+
+    private void RestartLevel(){
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        Debug.Log("Lever has restarted.");
+        PlayerBehavior.alive = true;
+        rb.bodyType = RigidbodyType2D.Dynamic;
     }
     // Update is called once per frame
     void Update()
