0de04dff7e314e1d436d73876130ebc9c42288c8
George-Seah <georgerseah@gmail.com>
2023-10-09 16:59:14 +0800

    Invulnerability frames post-hit added
    Player flashes with partly transparent and red when invulnerable after being hit.

--- 2D TEST/Assets/Scripts/PlayerHealth.cs
+++ 2D TEST/Assets/Scripts/PlayerHealth.cs
@@ -6,11 +6,17 @@ using UnityEngine.SceneManagement;
 
 public class PlayerHealth : MonoBehaviour
 {
+    [Header ("Health")]
     public int maxHealth = 5;
     public static int currentHealth = 3;
     
     private Rigidbody2D rb;
     private Animator anim;
+
+    [Header ("iFrames")]
+    [SerializeField] private float iFramesDuration;
+    [SerializeField] private int numberOfFlashes;
+    private SpriteRenderer spriteRend;
 
     // Start is called before the first frame update
     void Start()
@@ -18,11 +24,14 @@ public class PlayerHealth : MonoBehaviour
         currentHealth = 3;
         anim = GetComponent<Animator>();
         rb = GetComponent<Rigidbody2D>();
+        spriteRend = GetComponent<SpriteRenderer>();
     }
 
     public void TakeDamage(int amount){
         currentHealth -= amount;
-
+        if(currentHealth > 0){
+            StartCoroutine(Invulnerability());
+        }
         if(currentHealth <= 0){
             //Player is dead
             //Play Death Animation
@@ -57,6 +66,17 @@ public class PlayerHealth : MonoBehaviour
         Debug.Log("Lever has restarted.");
         PlayerBehavior.alive = true;
         rb.bodyType = RigidbodyType2D.Dynamic;
+    }
+
+    private IEnumerator Invulnerability(){
+        Physics2D.IgnoreLayerCollision(7, 8, true);
+        for(int i = 0; i < numberOfFlashes; i++){
+            spriteRend.color = new Color(1, 0, 0, 0.5f);
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+            spriteRend.color = Color.white;
+            yield return new WaitForSeconds(iFramesDuration / (numberOfFlashes * 2));
+        }
+        Physics2D.IgnoreLayerCollision(7, 8, false);
     }
     // Update is called once per frame
     void Update()
