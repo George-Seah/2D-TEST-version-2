6838c4fe69a2ba8f891a9610646b957417f512df
George-Seah <georgerseah@gmail.com>
2023-10-11 01:48:54 +0800

    Added more control over enemy hit detection for player stomp and added it so that stomping on enemy doesn't hurt player but makes them bounce a little.

--- 2D TEST/Assets/Scripts/EnemyDamage.cs
+++ 2D TEST/Assets/Scripts/EnemyDamage.cs
@@ -5,6 +5,7 @@ using UnityEngine;
 public class EnemyDamage : MonoBehaviour
 {
     public int damage = 1;
+    public bool damageGiving = true;
     private PlayerHealth playerHealth;
     private PlayerBehavior playerBehavior;
     //public PlayerMovement playerMovement;
@@ -20,7 +21,7 @@ public class EnemyDamage : MonoBehaviour
         
     }
     private void OnCollisionEnter2D(Collision2D collision){
-        if(collision.gameObject.tag == "Player"){
+        if(collision.gameObject.tag == "Player" && damageGiving){
             playerHealth = collision.gameObject.GetComponent<PlayerHealth>();
             playerBehavior = collision.gameObject.GetComponent<PlayerBehavior>();
             playerBehavior.KBCounter = playerBehavior.KBTotalTime;
--- 2D TEST/Assets/Scripts/EnemyWeakpoint.cs
+++ 2D TEST/Assets/Scripts/EnemyWeakpoint.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class EnemyWeakpoint : MonoBehaviour
 {
+    public Vector3 position;
     public Vector2 boxSize;
     public float castDistance;
     public LayerMask playerLayer;
     private EnemyDamage enemyDamage;
     // Start is called before the first frame update
     public bool isStomped(){
-        if(Physics2D.BoxCast(transform.position, boxSize, 0, transform.up, castDistance, playerLayer)){
+        if(Physics2D.BoxCast(transform.position + position, boxSize, 0, transform.up, castDistance, playerLayer)){
             return true;
         }
         else{
@@ -25,13 +26,7 @@ public class EnemyWeakpoint : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(isStomped()){
-            enemyDamage.enabled = false;
-            Debug.Log("enemyDamage.enabled = false");
-            Destroy(gameObject);
-            Debug.Log("Mask Dude should be killed.");
-        }
-    }
+
     /*
     private void OnCollisionEnter2D(Collision2D collision){
         if(isStomped()){
@@ -40,9 +35,30 @@ public class EnemyWeakpoint : MonoBehaviour
             Destroy(gameObject);
             Debug.Log("Mask Dude should be killed.");
         }
-    }
     */
+    }
+    
+
+    void OnCollisionEnter2D(Collision2D collision){
+        //collision.gameObject.GetComponent<Rigidbody2D>().velocity = new Vector2(collision.rigidbody.velocity.x, 5);
+
+        if(isStomped()){
+            enemyDamage.damageGiving = false;
+            Debug.Log("enemyDamage.damageGiving = false");
+            Destroy(gameObject);
+            Debug.Log("Mask Dude should be killed.");
+            /*
+            collision.rigidbody.velocity = new Vector2(collision.rigidbody.velocity.x, 15);
+            Debug.Log("Player should bounce off enemy.");
+            */
+            
+            //playerLayer.rigidbody.velocity = new Vector2(0f, 15f);
+            
+
+            
+        }
+    }
     private void OnDrawGizmos(){
-        Gizmos.DrawWireCube(transform.position+transform.up * castDistance, boxSize);
+        Gizmos.DrawWireCube((transform.position+position)+transform.up * castDistance, boxSize);
     }
 }
