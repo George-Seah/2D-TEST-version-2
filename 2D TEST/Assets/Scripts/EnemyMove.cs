80adacd160a01dda04da6622e840e54d89fe24a0
George-Seah <georgerseah@gmail.com>
2023-10-17 19:11:18 +0800

    Added a feature that makes Pink Men follow the player when they go within a certain radius.

--- 2D TEST/Assets/Scripts/EnemyMove.cs
+++ 2D TEST/Assets/Scripts/EnemyMove.cs
@@ -9,6 +9,10 @@ public class EnemyMove : MonoBehaviour
     public float speed;
     public int patrolDestination = 1;
 
+    public GameObject player;
+    public bool isChasing;
+    public float chaseDistance;
+
     private SpriteRenderer spriteRend;
 
     void Start(){
@@ -19,24 +23,45 @@ public class EnemyMove : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        if(patrolDestination == 1){
-            transform.position = Vector2.MoveTowards(transform.position, destination1, speed * Time.deltaTime);
-            if(Vector2.Distance(transform.position, destination1) < .2f){
-                patrolDestination = 2;
+        if(isChasing){
+            if(transform.position.x > player.transform.position.x){
+                //player.GetComponent<SpriteRenderer>().flipX = false;
+                spriteRend.flipX = true;
+                transform.position += Vector3.left * speed * Time.deltaTime;
+            }
+            if(transform.position.x < player.transform.position.x){
+                //player.GetComponent<SpriteRenderer>().flipX = true;
                 spriteRend.flipX = false;
+                transform.position += Vector3.right * speed * Time.deltaTime;
+            }
+        }
+        else{
+            if(Vector2.Distance(transform.position, player.transform.position) < chaseDistance){
+                isChasing = true;
+            }
+            if(patrolDestination == 1){
+                transform.position = Vector2.MoveTowards(transform.position, destination1, speed * Time.deltaTime);
+                if(Vector2.Distance(transform.position, destination1) < .2f){
+                    patrolDestination = 2;
+                    spriteRend.flipX = false;
+                }
+            }
+
+            else if(patrolDestination == 2){
+                transform.position = Vector2.MoveTowards(transform.position, destination2, speed * Time.deltaTime);
+                if(Vector2.Distance(transform.position, destination2) < .2f){
+                    patrolDestination = 1;
+                    spriteRend.flipX = true;
+                }
             }
         }
 
-        else if(patrolDestination == 2){
-            transform.position = Vector2.MoveTowards(transform.position, destination2, speed * Time.deltaTime);
-            if(Vector2.Distance(transform.position, destination2) < .2f){
-                patrolDestination = 1;
-                spriteRend.flipX = true;
-            }
-        }
     }
     private void OnDrawGizmos(){
+        Gizmos.color = Color.red;
         Gizmos.DrawWireCube(destination1, new Vector3(1, 1, 1));
         Gizmos.DrawWireCube(destination2, new Vector3(1, 1, 1));
+        Gizmos.color = Color.magenta;
+        Gizmos.DrawWireSphere(transform.position, chaseDistance);
     }
 }
